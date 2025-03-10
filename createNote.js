4403639d75e75d5b8269a1c3a617fe35baab0b2a
Eyal Levy <eyalevy23@gmail.com>
2025-03-10 17:11:35 +0200

    i am not sorry, add js logic to create note

---
+++ createNote.js
@@ -0,0 +1,97 @@
+export const getRandomElement = (arr) =>
+  arr[Math.floor(Math.random() * arr.length)];
+
+export const pastelColors = [
+  "#ffc", // Pastel Yellow
+  "#B2E0B2", // Pastel Green
+  "#A4C8E1", // Pastel Blue
+  "#FFB3C1", // Pastel Pink
+  "#E6B3E0", // Pastel Lavender
+  "#FFCCB3", // Pastel Peach
+  "#ccf", // Pastel Mint
+  "#cfc",
+];
+
+const degrees = [
+  "-rotate-7deg",
+  "-rotate-5deg",
+  "-rotate-2deg",
+  "rotate-2deg",
+  "rotate-5deg",
+  "rotate-7deg",
+];
+const positions = ["-top-1", "top-0", "top-1"];
+
+const pinPosition = ["", "transform: scaleX(-1)"];
+
+export const createNewNote = (noteObj) => {
+  function createTimeStamp() {
+    const details = document.createElement("div");
+    details.className = "text-xl";
+
+    const timestamp = [
+      { label: "Date:", value: noteObj.date },
+      { label: "Time:", value: noteObj.time },
+    ].map(({ label, value }) => {
+      const item = document.createElement("div");
+      item.className = "flex gap-3";
+
+      const labelSpan = document.createElement("span");
+      labelSpan.className = "label border-b-2 border-gray-300";
+      labelSpan.textContent = label;
+
+      const valueSpan = document.createElement("span");
+      valueSpan.textContent = value;
+
+      item.append(labelSpan, valueSpan);
+
+      return item;
+    });
+
+    details.append(...timestamp);
+    return details;
+  }
+
+  function createNoteContainer() {
+    const bgColor = getRandomElement(pastelColors);
+    const rotation = getRandomElement(degrees);
+    const position = getRandomElement(positions);
+
+    const note = document.createElement("div");
+    note.className = `note flex flex-col justify-between bg-[${noteObj.color}] ${rotation} ${position}`;
+
+    const content = document.createElement("div");
+    content.className = "overflow-y-auto";
+    const paragraph = document.createElement("p");
+    paragraph.textContent = noteObj.text;
+    content.appendChild(paragraph);
+    note.appendChild(content);
+    return note;
+  }
+
+  function createImg() {
+    const img = document.createElement("img");
+    img.className = "pin";
+    img.src = "/asset/image.svg";
+    img.style = getRandomElement(pinPosition);
+    return img;
+  }
+
+  function createNote() {
+    const div = document.createElement("div");
+    div.className = "relative";
+
+    const note = createNoteContainer();
+
+    const timestamp = createTimeStamp();
+
+    const img = createImg();
+
+    note.appendChild(timestamp);
+    div.append(img, note);
+
+    return div;
+  }
+
+  return createNote();
+};
--- script.js
+++ script.js
@@ -1,54 +1,35 @@
-const createNoteElement = () => {
-  const div = document.createElement("div");
-  div.className = "relative";
-
-  const note = document.createElement("div");
-  note.className =
-    "note flex flex-col justify-between bg-[#A4C8E1] rotate-[-7deg]";
-
-  const content = document.createElement("div");
-  content.className = "content max-h-30 overflow-y-auto";
-  const paragraph = document.createElement("p");
-  paragraph.textContent =
-    "# Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut doloribus reprehenderit nobis consectetur debitis libero ea explicabo alias ratione nulla incidunt odio repellat necessitatibus deserunt, ab et modi, vero repudiandae?";
-  content.appendChild(paragraph);
-
-  const details = document.createElement("div");
-  details.className = "details text-xl";
-
-  const values = ["10/02/2025", "10:00"];
-
-  // Assuming items is an array of elements you want to append
-  const timestamp = ["Date:", "Time:"].map((label, index) => {
-    const item = document.createElement("div");
-    item.className = `flex gap-3`;
-
-    const labelSpan = document.createElement("span");
-    labelSpan.className = "label border-b-2 border-gray-300";
-    labelSpan.textContent = label;
-
-    const valueSpan = document.createElement("span");
-    valueSpan.textContent = values[index];
-
-    item.append(labelSpan, valueSpan);
-
-    return item; // Return the created item
-  });
-
-  // Append all created items to the details container using the spread operator
-  details.append(...timestamp);
-
-  note.appendChild(content);
-  note.appendChild(details);
-  div.appendChild(note);
-
-  return div;
-};
+import { createNewNote, getRandomElement, pastelColors } from "./createNote.js";
 
 const init = () => {
   const container = document.getElementById("notes");
-  const noteElement = createNoteElement();
-  container.appendChild(noteElement);
+
+  for (let i = 0; i < 27; i++) {
+    const newNote = {
+      id: "8273823472834",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut doloribus reprehenderit nobis consectetur debitis libero ea explicabo alias ratione nulla incidunt odio repellat necessitatibus deserunt, ab et modi, vero repudiandae?",
+      date: "10/12/2025",
+      time: "10:30",
+      color: getRandomElement(pastelColors),
+    };
+    const note = createNewNote(newNote);
+    container.appendChild(note);
+  }
+
+  //   const div = document.createElement("div");
+  //   div.className = "relative";
+  //   div.innerHTML = `
+  //         <div class="note flex flex-col justify-between -top-1 bg-[#A4C8E1] -rotate-7">
+  //             <div class="max-h-30 overflow-y-auto">
+  //                 <p># Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut doloribus reprehenderit nobis consectetur debitis libero ea explicabo alias ratione nulla incidunt odio repellat necessitatibus deserunt, ab et modi, vero repudiandae?</p>
+  //             </div>
+  //             <div class="text-xl">
+  //                 <span class="flex gap-3"><span class="border-b-2 border-gray-300">Date:</span>10/02/2025</span>
+  //                 <span class="flex gap-3"><span class="border-b-2 border-gray-300">Time:</span>12:30</span>
+  //             </div>
+  //         </div>
+  //         <img class="pin" src="/asset/image.svg" alt="" />
+  //   `;
+  //   container.appendChild(div);
 };
 
 document.addEventListener("DOMContentLoaded", init);
