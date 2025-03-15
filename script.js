0d0800ed713aad529c3aa7181832e58481004c7a
Eyal Levy <eyalevy23@gmail.com>
2025-03-15 23:00:17 +0200

    gsap animation

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ import { FormManager } from "./src/formManger.js";
 const init = () => {
   // localStorage.clear();
   new FormManager();
+
+  gsap.from(".note", {
+    opacity: 0.1, // Start fully transparent
+    duration: 0.3, // Slightly faster animation
+    stagger: 0.3, // Slightly quicker stagger for smoother sequence
+    ease: "power2.out(1.2)", // Using back.out for a slight overshoot that settles into place
+  });
 };
 
 document.addEventListener("DOMContentLoaded", init);
